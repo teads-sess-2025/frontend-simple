385d7a15ebd7e07ac06bd924226dd0e9475dc948
jgosar <jgosar@outbrain.com>
2025-06-13 14:06:04 +0200

    Fixed note view

--- src/app/views/note/note.view.ts
+++ src/app/views/note/note.view.ts
@@ -1,4 +1,4 @@
-import { Component, inject, input, effect } from "@angular/core";
+import { Component, inject, input, effect, signal } from "@angular/core";
 import { Router } from "@angular/router";
 import { Note } from "src/app/types/note";
 import { HttpClient } from "@angular/common/http";
@@ -16,39 +16,47 @@ export class NoteView {
     private http = inject(HttpClient);
     private router = inject(Router)
 
-    note: Note = { title: '', text: '' };
+    note = signal<Note>({ title: '', text: '' });
 
     constructor(){
         effect(()=>{
             if (this.id() && this.id() !== 'new') {
                 this.http.get<Note>(
                     `${NOTES_BASE_URL}/${this.id()}`
-                ).subscribe(note => this.note = note);
+                ).subscribe(note => this.note.set(note));
             }
         })
     }
 
     saveChanges() {
-        if (this.note.id) {
+        if (this.note().id) {
             this.http.put<Note>(
-                `${NOTES_BASE_URL}/${this.note.id}`,
-                this.note
-            ).subscribe(note => this.note = note);
+                `${NOTES_BASE_URL}/${this.note().id}`,
+                this.note()
+            ).subscribe(note => this.note.set(note));
         } else {
             this.http.post<Note>(
                 NOTES_BASE_URL,
-                this.note
+                this.note()
             ).subscribe(note => this.router.navigateByUrl(`/note/${note.id}`));
         }
     }
 
     deleteNote() {
-        if (this.note.id && window.confirm("Are you sure you want to delete this note?")) {
+        if (this.note().id && window.confirm("Are you sure you want to delete this note?")) {
             this.http.delete<void>(
-                `${NOTES_BASE_URL}/${this.note.id}`,
+                `${NOTES_BASE_URL}/${this.note().id}`,
             ).subscribe(() => this.router.navigateByUrl('/'));
         } else {
             this.router.navigateByUrl('/');
         }
+    }
+
+    updateTitle(title: string) {
+        this.note.update(current => ({ ...current, title }));
+    }
+
+    updateText(text: string) {
+        this.note.update(current => ({ ...current, text }));
     }
 }
